a55c8fc4b2c6c968d99f9ae2f5a21d6aae070e34
Pavel Volokitin <pvolokitin@rvdsystems.ru>
2021-10-19 00:37:18 +0300

    Description

--- src/main/java/com/example/demo/json/WildberriesCategory.java
+++ src/main/java/com/example/demo/json/WildberriesCategory.java
@@ -11,30 +11,6 @@ public class WildberriesCategory {
         this.name = name;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(long parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
         return "WildberriesCategory{" +
